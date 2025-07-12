332f80620fc3a05b8f495aa1594ca07fab5472a0
jubayer718 <jubayerahmed2462@gmail.com>
2025-07-12 07:51:44 +0600

    review created.

--- src/app/controllers/review.controller.ts
+++ src/app/controllers/review.controller.ts
@@ -1,3 +1,36 @@
 import express, { Request, Response } from 'express';
+import ReviewModel from '../models/review';
+
+
 
 export const reviewRouter = express.Router();
+
+
+reviewRouter.post('/reviews', async (req: Request, res: Response, next) => {
+  try {
+    const body = await req.body;
+    const result = await ReviewModel.create(body);
+    res.json({
+      success: true,
+      message: 'review added',
+      data: result
+    })
+  } catch (error) {
+    next(error)
+  }
+})
+
+
+reviewRouter.get('/reviews/:id', async (req: Request, res: Response, next) => {
+  try {
+    const collegeId = req.params;
+    const review = await ReviewModel.findById(collegeId);
+    res.json({
+      success: true,
+      message: "review",
+      data: review
+    })
+  } catch (error) {
+    next(error)
+  }
+})
